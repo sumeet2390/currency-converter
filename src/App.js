ec22e1fc0d1b842cf81cdad24ee177c9633ab98e
Sumeet Suryawanshi <sumeetsuryawanshi@Sumeets-MacBook-Pro.local>
2020-01-12 23:32:08 +0530

    some styling using flexbox and styled component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import "./App.css";
 import InputPanel from "./components/InputPanel";
 import OutputPanel from "./components/OutputPanel";
@@ -7,17 +8,28 @@ import LiveRateContainer from "./components/LiveRateContainer";
 import OutputCurrencyDropdown from "./components/output-currency-dropdown/OutputCurrencyDropdown";
 import InputCurrencyDropdown from "./components/input-currency-dropdown/InputCurrencyDropdown";
 
+const DropdownWithInput = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 10px;
+`;
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <LiveRateContainer />
 
-        <InputCurrencyDropdown />
-        <InputPanel />
-
-        <OutputCurrencyDropdown />
-        <OutputPanel />
+        <DropdownWithInput>
+          <InputCurrencyDropdown />
+          <InputPanel />
+        </DropdownWithInput>
+        <div className="separator"></div>
+        <DropdownWithInput>
+          <OutputCurrencyDropdown />
+          <OutputPanel />
+        </DropdownWithInput>
       </div>
     );
   }
