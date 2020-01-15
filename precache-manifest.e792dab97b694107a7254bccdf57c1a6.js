8b9ea2236a65f60c6d929e0bdf61bd2678c6d4b9
Sumeet Suryawanshi <sumeetsuryawanshi@Sumeets-MacBook-Pro.local>
2020-01-15 13:40:30 +0530

    Updates

---
+++ precache-manifest.e792dab97b694107a7254bccdf57c1a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b766976f7344bf68fbce6bbb60853e",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "366d22770dcf602661d6",
+    "url": "/currency-converter/static/css/main.0c069bf0.chunk.css"
+  },
+  {
+    "revision": "eb73722f69ed8e2e744e",
+    "url": "/currency-converter/static/js/2.c48d180e.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/currency-converter/static/js/2.c48d180e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "366d22770dcf602661d6",
+    "url": "/currency-converter/static/js/main.15e782ac.chunk.js"
+  },
+  {
+    "revision": "facce7c67e50e2c95d26",
+    "url": "/currency-converter/static/js/runtime-main.e7e16271.js"
+  },
+  {
+    "revision": "b67dc7662761051730f12ff42c4340a0",
+    "url": "/currency-converter/static/media/fa-angle-down.b67dc766.svg"
+  },
+  {
+    "revision": "e1241f4a86fdd8229ca857752a52861a",
+    "url": "/currency-converter/static/media/repeat.e1241f4a.svg"
+  }
+]);
